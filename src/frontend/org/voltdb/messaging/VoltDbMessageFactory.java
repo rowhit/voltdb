bf4a6c844cd8098916003df6552a151f69e52fe4
Dongjun Sun <dsun@voltdb.com>
2016-07-27 22:11:55 -0400

    ENG-10524: Coordinate @ApplyBinaryLogMPs globally
    Initial changes include:
    * Coolocate MP consumer coodinator with MPI
    * Have DRConsumerDrIdTracker carry producer partition id
    * Skeleton logic for split/combine @ApplyBinaryLogMPsA

--- src/frontend/org/voltdb/messaging/VoltDbMessageFactory.java
+++ src/frontend/org/voltdb/messaging/VoltDbMessageFactory.java
@@ -47,6 +47,7 @@ public class VoltDbMessageFactory extends VoltMessageFactory
     final public static byte SNAPSHOT_CHECK_REQUEST_ID = VOLTCORE_MESSAGE_ID_MAX + 21;
     final public static byte SNAPSHOT_CHECK_RESPONSE_ID = VOLTCORE_MESSAGE_ID_MAX + 22;
     final public static byte IV2_REPAIR_LOG_TRUNCATION = VOLTCORE_MESSAGE_ID_MAX + 23;
+    final public static byte DR2_MULTIPART_TASK_ID = VOLTCORE_MESSAGE_ID_MAX + 24;
 
     /**
      * Overridden by subclasses to create message types unknown by voltcore
@@ -128,6 +129,9 @@ public class VoltDbMessageFactory extends VoltMessageFactory
             break;
         case IV2_REPAIR_LOG_TRUNCATION:
             message = new RepairLogTruncationMessage();
+            break;
+        case DR2_MULTIPART_TASK_ID:
+            message = new Dr2MultipartTaskMessage();
             break;
         default:
             message = null;
