f958e5d85a8cf96ccd17bd4ca0c4f6c8f9b35ac9
Paul Martel <pmartel@voltdb.com>
2016-08-09 09:48:06 -0400

    ENG-10740 planner performance and simplification

--- tests/ee/executors/TestGeneratedPlans.cpp
+++ tests/ee/executors/TestGeneratedPlans.cpp
@@ -1,45 +1,4 @@
-/* This file is part of VoltDB.
- * Copyright (C) 2008-2016 VoltDB Inc.
- *
- * This file contains original code and/or modifications of original code.
- * Any modifications made by VoltDB Inc. are licensed under the following
- * terms and conditions:
- *
- * Permission is hereby granted, free of charge, to any person obtaining
- * a copy of this software and associated documentation files (the
- * "Software"), to deal in the Software without restriction, including
- * without limitation the rights to use, copy, modify, merge, publish,
- * distribute, sublicense, and/or sell copies of the Software, and to
- * permit persons to whom the Software is furnished to do so, subject to
- * the following conditions:
- *
- * The above copyright notice and this permission notice shall be
- * included in all copies or substantial portions of the Software.
- *
- * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
- * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
- * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
- * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
- * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
- * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
- * OTHER DEALINGS IN THE SOFTWARE.
- */
-/**
- * Licensed to the Apache Software Foundation (ASF) under one or more
- * contributor license agreements. See the NOTICE file distributed with this
- * work for additional information regarding copyright ownership. The ASF
- * licenses this file to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
- * WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
- * License for the specific language governing permissions and limitations under
- * the License.
- */
+
 /******************************************************************************************
  *
  * NOTA BENE: This file is automagically generated from the source class named
@@ -572,9 +531,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV httpdportno 0\n"
     "set $PREV jsonapi false\n"
     "set $PREV networkpartition false\n"
-    "set $PREV voltRoot \"\"\n"
-    "set $PREV exportOverflow \"\"\n"
-    "set $PREV drOverflow \"\"\n"
     "set $PREV adminport 0\n"
     "set $PREV adminstartup false\n"
     "set $PREV heartbeatTimeout 0\n"
@@ -619,7 +575,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV name \"A\"\n"
     "set $PREV defaultvalue null\n"
     "set $PREV defaulttype 0\n"
-    "set $PREV matview null\n"
     "set $PREV aggregatetype 0\n"
     "set $PREV matviewsource null\n"
     "set $PREV inbytes false\n"
@@ -631,7 +586,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV name \"B\"\n"
     "set $PREV defaultvalue null\n"
     "set $PREV defaulttype 0\n"
-    "set $PREV matview null\n"
     "set $PREV aggregatetype 0\n"
     "set $PREV matviewsource null\n"
     "set $PREV inbytes false\n"
@@ -643,7 +597,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV name \"C\"\n"
     "set $PREV defaultvalue null\n"
     "set $PREV defaulttype 0\n"
-    "set $PREV matview null\n"
     "set $PREV aggregatetype 0\n"
     "set $PREV matviewsource null\n"
     "set $PREV inbytes false\n"
@@ -663,7 +616,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV name \"A\"\n"
     "set $PREV defaultvalue null\n"
     "set $PREV defaulttype 0\n"
-    "set $PREV matview null\n"
     "set $PREV aggregatetype 0\n"
     "set $PREV matviewsource null\n"
     "set $PREV inbytes false\n"
@@ -675,7 +627,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV name \"B\"\n"
     "set $PREV defaultvalue null\n"
     "set $PREV defaulttype 0\n"
-    "set $PREV matview null\n"
     "set $PREV aggregatetype 0\n"
     "set $PREV matviewsource null\n"
     "set $PREV inbytes false\n"
@@ -687,7 +638,6 @@ DBConfig TestGeneratedPlans::m_PartitionByExecutorDB =
     "set $PREV name \"C\"\n"
     "set $PREV defaultvalue null\n"
     "set $PREV defaulttype 0\n"
-    "set $PREV matview null\n"
     "set $PREV aggregatetype 0\n"
     "set $PREV matviewsource null\n"
     "set $PREV inbytes false\n"
